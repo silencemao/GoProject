544a0ce0a03c96b30150b7bcdddaa19d32109541
silencemao <17862953895@163.com>
2021-06-28 22:41:16 +0800

    find peak num

--- lc/top200/lc_162_findPeakElement.go
+++ lc/top200/lc_162_findPeakElement.go
@@ -25,6 +25,15 @@ func findPeakElement(nums []int) int {
 	return l
 }
 
+func findPeakElement1(nums []int) int {
+	for i := 0; i < len(nums)-1; i++ {
+		if nums[i] > nums[i+1] { // 不存在重复元素
+			return i
+		}
+	}
+	return len(nums) - 1
+}
+
 /*
 
 class Solution {
