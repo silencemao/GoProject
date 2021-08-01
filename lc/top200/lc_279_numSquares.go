67ddc731f41876621f4086cde687837d3ccfba14
silencemao <17862953895@163.com>
2021-08-01 16:47:53 +0800

    numsquare

--- lc/top200/lc_279_numSquares.go
+++ lc/top200/lc_279_numSquares.go
@@ -20,13 +20,15 @@ n=3 由3个完全平方数组成
 组成n的完全平方数的范围肯定在 [1-n/2]的范围内（确切的说应该是[0, sqrt(n)]）
 遍历j [1-n/2]，若该数的平方小于n，则j*j可以是组成n的一个完全平方数，而在n中要腾出j*j的空间，即n-j*j，
 我们查看组成n-j*j最少需要几个完全平方数，即转移方程为dp[n]=min(dp[n-j*j], dp[n])
+
+类似于322题 coin change
 */
 func numSquares(n int) int {
 	dp := make([]int, n+1)
 	dp[1] = 1
 	for i := 2; i <= n; i++ {
 		dp[i] = 1<<31 - 1
-		for j := 1; j <= i/2; j++ {
+		for j := 1; j*j <= i; j++ {
 			if j*j <= i {
 				dp[i] = util.MinInt(dp[i-j*j]+1, dp[i])
 			}
