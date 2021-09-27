8ecaa93867d1cfd1c9db162c4a31cb550630ae01
silencemao <17862953895@163.com>
2021-09-27 23:38:59 +0800

    update

--- lc/top200/lc_121_maxProfit.go
+++ lc/top200/lc_121_maxProfit.go
@@ -8,7 +8,7 @@ import (
 // 只能交易一次
 func maxProfit(prices []int) int {
 	res := 0
-
+	// 以第i天结尾 交易一次 可以获得收益
 	for i := 1; i < len(prices); i++ {
 		for j := 0; j < i; j++ {
 			res = util.MaxInt(res, prices[i]-prices[j])
@@ -19,9 +19,9 @@ func maxProfit(prices []int) int {
 
 func maxProfit1(prices []int) int {
 	res := 0
-	dp := make([]int, len(prices))
+	dp := make([]int, len(prices)) // dp[i]存储第i天卖出时时 可获得的最大收益
 	for i := 1; i < len(prices); i++ {
-		dp[i] = util.MaxInt(dp[i-1]+prices[i]-prices[i-1], 0)
+		dp[i] = util.MaxInt(dp[i-1]+prices[i]-prices[i-1], 0) // 这种写法可以累加收益，即在昨天的基础上我是否还能获得更多收益
 		res = util.MaxInt(dp[i], res)
 	}
 	return res
