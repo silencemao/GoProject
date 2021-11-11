0762b7b2e7a434e60c1fc1b9857703bff2cd79e6
silencemao <17862953895@163.com>
2021-11-11 22:23:57 +0800

    add:: reverse List

--- lc/top200/lc_206_reverseList.go
+++ lc/top200/lc_206_reverseList.go
@@ -16,6 +16,34 @@ func reverseList(head *_struct.ListNode) *_struct.ListNode {
 	return pre
 }
 
+func help206(pre, cur *_struct.ListNode) *_struct.ListNode {
+	if cur == nil {
+		return pre
+	}
+	tmp := cur.Next
+	cur.Next = pre
+
+	return help206(cur, tmp)
+}
+
+func reverseList1(head *_struct.ListNode) *_struct.ListNode {
+	return help206(nil, head)
+}
+
+func reverseList2(head *_struct.ListNode) *_struct.ListNode {
+	var pre *_struct.ListNode // 只声明类型 不初始化
+	cur := head
+	for cur != nil {
+		tmp := cur.Next
+
+		cur.Next = pre
+
+		pre = cur
+		cur = tmp
+	}
+	return pre
+}
+
 func main() {
 	l1 := &_struct.ListNode{Val: 1, Next: &_struct.ListNode{Val: 4, Next: &_struct.ListNode{Val: 5}}}
 	_struct.PrintList(l1)
