5dd1326c480cbf1bbe9993666cce8e901e1b6799
silencemao <17862953895@163.com>
2021-12-06 23:15:50 +0800

    add::

--- lc/tree/solution/lc_405_sumOfLeftLeaves.go
+++ lc/tree/solution/lc_405_sumOfLeftLeaves.go
@@ -1,18 +1,28 @@
 package main
 
 import (
-	"fmt"
 	"GoProject/leetcode/tree"
+	"fmt"
 )
 
 func sumOfLeftLeaves(root *tree.TreeNode) int {
 	if root == nil {
 		return 0
 	}
-	if root.Left != nil && root.Left.Left==nil && root.Left.Right==nil {
+	if root.Left != nil && root.Left.Left == nil && root.Left.Right == nil {
 		return root.Left.Val + sumOfLeftLeaves(root.Right)
 	}
 	return sumOfLeftLeaves(root.Left) + sumOfLeftLeaves(root.Right)
+}
+
+func sumOfLeaves(root *tree.TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	if root.Left == nil && root.Right == nil {
+		return root.Val
+	}
+	return sumOfLeaves(root.Left) + sumOfLeaves(root.Right)
 }
 
 /*
@@ -28,5 +38,6 @@ func sumOfLeftLeaves(root *tree.TreeNode) int {
 
 func main() {
 	tRoot := tree.CreateTreeNode([]int{5, 4, 8, 11, tree.NilNode, 13, 4, 7, 2, tree.NilNode, tree.NilNode, tree.NilNode, tree.NilNode, tree.NilNode, 1})
-	fmt.Println(sumOfLeftLeaves(tRoot))
+	//fmt.Println(sumOfLeftLeaves(tRoot))
+	fmt.Println(sumOfLeaves(tRoot))
 }
