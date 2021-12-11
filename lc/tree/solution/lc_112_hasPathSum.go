7948b1098c41ffc41e42be51271b5aa395f3b22d
silencemao <17862953895@163.com>
2021-12-11 13:33:03 +0800

    add:: haspathsu

--- lc/tree/solution/lc_112_hasPathSum.go
+++ lc/tree/solution/lc_112_hasPathSum.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"GoProject/leetcode/tree"
+	"fmt"
 )
 
 func hasPathSum(root *tree.TreeNode, sum int) bool {
@@ -12,7 +12,27 @@ func hasPathSum(root *tree.TreeNode, sum int) bool {
 	if root.Left == nil && root.Right == nil {
 		return sum == root.Val
 	}
-	return hasPathSum(root.Left, sum - root.Val) || hasPathSum(root.Right, sum - root.Val)
+	return hasPathSum(root.Left, sum-root.Val) || hasPathSum(root.Right, sum-root.Val)
+}
+
+func hasPathSum1(root *tree.TreeNode, sum int) bool {
+	if root.Left == nil && root.Right == nil && sum == 0 {
+		return true
+	}
+	if root.Left == nil && root.Right == nil && sum != 0 {
+		return false
+	}
+	if root.Left != nil {
+		if hasPathSum1(root.Left, sum-root.Left.Val) {
+			return true
+		}
+	}
+	if root.Right != nil {
+		if hasPathSum1(root.Right, sum-root.Right.Val) {
+			return true
+		}
+	}
+	return false
 }
 
 /*
@@ -28,4 +48,5 @@ func hasPathSum(root *tree.TreeNode, sum int) bool {
 func main() {
 	tRoot := tree.CreateTreeNode([]int{5, 4, 8, 11, tree.NilNode, 13, 4, 7, 2, tree.NilNode, 1})
 	fmt.Println(hasPathSum(tRoot, 22))
+	fmt.Println(hasPathSum1(tRoot, 22))
 }
