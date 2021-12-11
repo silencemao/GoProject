16268d5c2aa737145a21b0f3a738abdc7c5996d3
silencemao <17862953895@163.com>
2021-12-11 13:38:57 +0800

    add:: sumOfleaves

--- lc/tree/solution/lc_405_sumOfLeftLeaves.go
+++ lc/tree/solution/lc_405_sumOfLeftLeaves.go
@@ -15,6 +15,45 @@ func sumOfLeftLeaves(root *tree.TreeNode) int {
 	return sumOfLeftLeaves(root.Left) + sumOfLeftLeaves(root.Right)
 }
 
+func sumOfLeftLeaves1(root *tree.TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	leftValue := sumOfLeftLeaves1(root.Left)
+	rightValue := sumOfLeftLeaves1(root.Right)
+
+	leaveNum := 0
+	if root.Left != nil && root.Left.Left == nil && root.Left.Right == nil {
+		leaveNum = root.Left.Val
+	}
+	sum := leaveNum + leftValue + rightValue
+	return sum
+}
+
+func sumOfLeftLeaves2(root *tree.TreeNode) int {
+	var stack []*tree.TreeNode
+	if root == nil {
+		return 0
+	}
+	res := 0
+	stack = append(stack, root)
+	for len(stack) > 0 {
+		top := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+
+		if top.Left != nil && top.Left.Left == nil && top.Left.Right == nil {
+			res += top.Left.Val
+		}
+		if top.Right != nil {
+			stack = append(stack, top.Right)
+		}
+		if top.Left != nil {
+			stack = append(stack, top.Left)
+		}
+	}
+	return res
+}
+
 func sumOfLeaves(root *tree.TreeNode) int {
 	if root == nil {
 		return 0
@@ -38,6 +77,8 @@ func sumOfLeaves(root *tree.TreeNode) int {
 
 func main() {
 	tRoot := tree.CreateTreeNode([]int{5, 4, 8, 11, tree.NilNode, 13, 4, 7, 2, tree.NilNode, tree.NilNode, tree.NilNode, tree.NilNode, tree.NilNode, 1})
-	//fmt.Println(sumOfLeftLeaves(tRoot))
-	fmt.Println(sumOfLeaves(tRoot))
+	fmt.Println(sumOfLeftLeaves(tRoot))
+	fmt.Println(sumOfLeftLeaves1(tRoot))
+	fmt.Println(sumOfLeftLeaves2(tRoot))
+	//fmt.Println(sumOfLeaves(tRoot))
 }
