77565de7c488c8035c150aba225ac10a9153706a
silencemao <17862953895@163.com>
2021-12-15 23:29:01 +0800

    update

--- lc/tree/solution/lc_617_mergeTrees.go
+++ lc/tree/solution/lc_617_mergeTrees.go
@@ -1,24 +1,34 @@
 package main
 
-import "GoProject/leetcode/tree"
+import (
+	"GoProject/leetcode/tree"
+	"fmt"
+)
 
 func mergeTrees(root1 *tree.TreeNode, root2 *tree.TreeNode) *tree.TreeNode {
 	if root1 == nil && root2 == nil {
 		return nil
 	}
 	if root1 == nil {
-		return &tree.TreeNode{Val: root2.Val}
+		return root2
+		//return &tree.TreeNode{Val: root2.Val}
 	}
 	if root2 == nil {
-		return &tree.TreeNode{Val: root1.Val}
+		return root1
+		//return &tree.TreeNode{Val: root1.Val}
 	}
 
 	root := &tree.TreeNode{Val: root1.Val + root2.Val}
-	root.Left = mergeTrees(root1.Left, root2.Left)
+	root.Left = mergeTrees(root1.Left, root2.Left) // 上面注释处的写法就结束了递归，则进入求解right(下一行)，同样下一行结束后就返回了，这样结果就不对
 	root.Right = mergeTrees(root1.Right, root2.Right)
 	return root
 }
 
 func main() {
+	tRoot1 := tree.CreateTreeNode([]int{1, 2, tree.NilNode, 3, tree.NilNode, tree.NilNode, tree.NilNode})
+	tRoot2 := tree.CreateTreeNode([]int{1, tree.NilNode, 2, tree.NilNode, tree.NilNode, tree.NilNode, 3})
 
+	res := mergeTrees(tRoot1, tRoot2)
+	fmt.Println(res.Val)
+	//levelOrderBottom(res)
 }
