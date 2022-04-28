9979ac8e7ee2527e611ebaf6c9e05257db74c303
silencemao <17862953895@163.com>
2022-04-28 23:18:40 +0800

    two sum

---
+++ lc/bs/lc_167_twoSum.go
@@ -0,0 +1,30 @@
+package main
+
+/*
+给你一个下标从 1 开始的整数数组numbers ，该数组已按 非递减顺序排列 ，请你从数组中找出满足相加之和等于目标数target 的两个数。
+如果设这两个数分别是 numbers[index1] 和 numbers[index2] ，则 1 <= index1 < index2 <= numbers.length 。
+以长度为 2 的整数数组 [index1, index2] 的形式返回这两个整数的下标 index1 和 index2。
+你可以假设每个输入 只对应唯一的答案 ，而且你 不可以 重复使用相同的元素。
+
+两数之和，数组有序，返回满足target的索引，
+索引从1 开始
+*/
+
+func twoSum(nums []int, target int) []int {
+	l, r := 0, len(nums)-1
+	for l <= r {
+		sum := nums[r] + nums[l]
+		if sum == target {
+			return []int{l + 1, r + 1}
+		} else if sum > target {
+			r -= 1
+		} else {
+			l += 1
+		}
+	}
+	return []int{-1, -1}
+}
+
+func main() {
+
+}
