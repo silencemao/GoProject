77fd0e794a394393733fd1ab0294c865fef6d614
silencemao <17862953895@163.com>
2022-04-29 08:21:19 +0800

    search matrix

---
+++ lc/bs/lc_240_searchMatrix.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+/*
+编写一个高效的算法来搜索mxn矩阵 matrix 中的一个目标值 target 。该矩阵具有以下特性：
+
+每行的元素从左到右升序排列。
+每列的元素从上到下升序排列。
+
+类似第34
+从右上角开始查找
+*/
+func searchMatrix240(matrix [][]int, target int) bool {
+	m, n := len(matrix), len(matrix[0])
+	i, j := 0, n-1
+	for i < m && j >= 0 {
+		if matrix[i][j] == target {
+			return true
+		} else if matrix[i][j] > target {
+			j--
+		} else {
+			i++
+		}
+	}
+
+	return false
+}
+
+func main() {
+	matrix := [][]int{{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19}, {3, 6, 9, 16, 22}, {10, 13, 14, 17, 24}, {18, 21, 23, 26, 30}}
+	target := 11
+	fmt.Println(searchMatrix240(matrix, target))
+}
