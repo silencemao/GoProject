209f12b55d0f57c98cda40d918dc3c673d68dfbf
silencemao <17862953895@163.com>
2022-04-30 16:00:48 +0800

    find suplicate

--- lc/bs/lc_287_findDuplicate.go
+++ lc/bs/lc_287_findDuplicate.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 /*
 给定一个包含n + 1 个整数的数组nums ，其数字都在[1, n]范围内（包括 1 和 n），可知至少存在一个重复的整数。
 假设 nums 只有 一个重复的整数 ，返回这个重复的数 。
@@ -20,6 +22,27 @@ func findDuplicate(nums []int) int {
 	return s
 }
 
-func main() {
+// 二分查找
+func findDuplicate1(nums []int) int {
+	l, r := 1, len(nums)-1 // 数组长度为n+1,数字范围是1-n，此时的l r 不是索引，而是数值
+	for l < r {
+		mid := l + (r-l)>>1
+		cnt := 0
+		for _, num := range nums {
+			if num <= mid {
+				cnt += 1
+			}
+		}
+		if cnt > mid {
+			r = mid
+		} else {
+			l = mid + 1
+		}
+	}
+	return l
+}
 
+func main() {
+	nums := []int{1, 3, 4, 2, 2}
+	fmt.Println(findDuplicate1(nums))
 }
