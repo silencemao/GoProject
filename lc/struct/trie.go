247ac120956ceda5bb241daeffa9db44eb8c16eb
silencemao <17862953895@163.com>
2022-06-11 17:50:12 +0800

    find words

--- lc/struct/trie.go
+++ lc/struct/trie.go
@@ -4,14 +4,14 @@ package _struct
 前缀树，字典树
 */
 type Trie struct {
-	isEnd bool
-	next  []*Trie
+	IsEnd bool
+	Next  []*Trie
 }
 
 func Constructor() Trie {
 	trie := Trie{}
-	trie.isEnd = false
-	trie.next = make([]*Trie, 26)
+	trie.IsEnd = false
+	trie.Next = make([]*Trie, 26)
 	return trie
 }
 
@@ -19,32 +19,32 @@ func (this *Trie) Insert(word string) {
 	node := this
 	for _, c := range word {
 		ch := c - 'a'
-		if node.next[ch] == nil {
+		if node.Next[ch] == nil {
 			t := Constructor()
-			node.next[ch] = &t
+			node.Next[ch] = &t
 		}
-		node = node.next[ch]
+		node = node.Next[ch]
 	}
-	node.isEnd = true
+	node.IsEnd = true
 }
 
 func (this *Trie) Search(word string) bool {
 	node := this
 	for _, c := range word {
 		ch := c - 'a'
-		node = node.next[ch]
+		node = node.Next[ch]
 		if node == nil {
 			return false
 		}
 	}
-	return node.isEnd
+	return node.IsEnd
 }
 
 func (this *Trie) StartsWith(prefix string) bool {
 	node := this
 	for _, c := range prefix {
 		ch := c - 'a'
-		node = node.next[ch]
+		node = node.Next[ch]
 		if node == nil {
 			return false
 		}
