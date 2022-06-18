93039d80c042a05a20529f07593fc25bb87a1bb5
silencemao <17862953895@163.com>
2022-06-18 17:37:46 +0800

    topkFrequent

--- lc/top200/lc_347_topKFrequent.go
+++ lc/top200/lc_347_topKFrequent.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"container/heap"
 	"fmt"
 	"sort"
 )
@@ -27,6 +28,42 @@ func topKFrequent(nums []int, k int) []int {
 
 	return tmp[:k]
 }
+
+func topKFrequentI(nums []int, k int) []int {
+	set := make(map[int]int, 0)
+	for _, n := range nums {
+		set[n] += 1
+	}
+	h := &hp1{}
+
+	for w, c := range set {
+		heap.Push(h, pair1{n: w, c: c})
+
+		if h.Len() > k {
+			heap.Pop(h)
+		}
+	}
+	res := make([]int, k)
+	for i := k - 1; i >= 0; i-- {
+		res[i] = heap.Pop(h).(pair1).n
+	}
+	return res
+}
+
+type pair1 struct {
+	n int
+	c int
+}
+
+type hp1 []pair1
+
+func (h hp1) Len() int           { return len(h) }
+func (h hp1) Less(i, j int) bool { return h[i].c < h[j].c }
+func (h hp1) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *hp1) Pop() interface{}   { a := *h; v := a[len(a)-1]; *h = a[:len(a)-1]; return v }
+func (h *hp1) Push(v interface{}) { *h = append(*h, v.(pair1)) }
+
 func main() {
 	nums := []int{1, 1, 1, 2, 2, 3}
 	k := 2
