73a2ed67144850386ce450c28d1aa8c751a452da
silencemao <17862953895@163.com>
2022-07-06 00:01:23 +0800

    isInterLeave

---
+++ lc/dp/lc_97_isInterleave.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+/*
+给定三个字符串s1、s2、s3，请你帮忙验证s3是否是由s1和s2 交错 组成的。
+
+两个字符串 s 和 t 交错 的定义与过程如下，其中每个字符串都会被分割成若干 非空 子字符串：
+
+s = s1 + s2 + ... + sn
+t = t1 + t2 + ... + tm
+|n - m| <= 1
+交错 是 s1 + t1 + s2 + t2 + s3 + t3 + ... 或者 t1 + s1 + t2 + s2 + t3 + s3 + ...
+注意：a + b 意味着字符串 a 和 b 连接。
+*/
+func isInterleave(s1 string, s2 string, s3 string) bool {
+	m, n, k := len(s1), len(s2), len(s3)
+	if m+n != k {
+		return false
+	}
+	dp := make([][]bool, m+1)
+	for i := range dp {
+		dp[i] = make([]bool, n+1)
+	}
+	dp[0][0] = true
+	for i := 0; i <= m; i++ {
+		for j := 0; j <= n; j++ {
+			p := i + j - 1
+			if i > 0 {
+				dp[i][j] = dp[i][j] || (dp[i-1][j] && s1[i-1] == s3[p])
+			}
+			if j > 0 {
+				dp[i][j] = dp[i][j] || (dp[i][j-1] && s2[j-1] == s3[p])
+			}
+		}
+	}
+	return dp[m][n]
+}
+
+func main() {
+	s1, s2, s3 := "aabcc", "dbbca", "aadbbcbcac"
+	fmt.Println(isInterleave(s1, s2, s3))
+}
