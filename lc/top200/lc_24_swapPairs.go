6c90a33524cb7d57a85f74fc89f1a3a531d45572
silencemao <17862953895@163.com>
2022-07-07 08:38:24 +0800

    swappairs

--- lc/top200/lc_24_swapPairs.go
+++ lc/top200/lc_24_swapPairs.go
@@ -1,6 +1,7 @@
 package main
 
 import _struct "GoProject/leetcode/struct"
+
 /*
 两两交换链表中的节点
 pre cur next
@@ -28,9 +29,33 @@ func swapPairs(head *_struct.ListNode) *_struct.ListNode {
 	return dummy.Next
 }
 
+func swapPairs1(head *_struct.ListNode) *_struct.ListNode {
+	dummy := &_struct.ListNode{Val: -1, Next: head}
+
+	pre, cur, next := dummy, head, head.Next
+	for next != nil {
+		tmp := next.Next
+
+		cur.Next = tmp
+		next.Next = cur
+		pre.Next = next
+
+		pre = cur
+		cur = tmp
+		if tmp == nil {
+			break
+		}
+		next = tmp.Next
+	}
+
+	return dummy.Next
+}
+
 func main() {
-	head := &_struct.ListNode{Val: 1, Next: &_struct.ListNode{Val: 2, Next: &_struct.ListNode{Val: 3,Next: &_struct.ListNode{Val: 4}}}}
-	head = swapPairs(head)
+	head := &_struct.ListNode{Val: 1, Next: &_struct.ListNode{Val: 2, Next: &_struct.ListNode{Val: 3, Next: &_struct.ListNode{Val: 4}}}}
+	//head = swapPairs(head)
+	_struct.PrintList(head)
+	head = swapPairs1(head)
 
 	_struct.PrintList(head)
 }
