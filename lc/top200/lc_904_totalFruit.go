8624db69de63d5b5924bda2e4c450a76ce40bd2b
silencemao <17862953895@163.com>
2022-10-18 08:27:09 +0800

    update

---
+++ lc/top200/lc_904_totalFruit.go
@@ -0,0 +1,43 @@
+package main
+
+import "GoProject/leetcode/util"
+
+/*
+你正在探访一家农场，农场从左到右种植了一排果树。这些树用一个整数数组 fruits 表示，其中 fruits[i] 是第 i 棵树上的水果 种类 。
+
+你想要尽可能多地收集水果。然而，农场的主人设定了一些严格的规矩，你必须按照要求采摘水果：
+
+你只有 两个 篮子，并且每个篮子只能装 单一类型 的水果。每个篮子能够装的水果总量没有限制。
+你可以选择任意一棵树开始采摘，你必须从 每棵 树（包括开始采摘的树）上 恰好摘一个水果 。采摘的水果应当符合篮子中的水果类型。
+每采摘一次，你将会向右移动到下一棵树，并继续采摘。
+一旦你走到某棵树前，但水果不符合篮子的水果类型，那么就必须停止采摘。
+给你一个整数数组 fruits ，返回你可以收集的水果的 最大 数目。
+
+输入：fruits = [1,2,1]
+输出：3
+解释：可以采摘全部 3 棵树。
+
+最长无重复字符连续子数组的变体，即包含两种元素的连续子数组的最大长度
+
+*/
+func totalFruit(fruits []int) int {
+	res := 0
+	set := map[int]int{}
+	l, r := 0, 0
+	for ; r < len(fruits); r++ {
+		set[fruits[r]] += 1
+		for len(set) > 2 {
+			set[fruits[l]] -= 1
+			if set[fruits[l]] == 0 {
+				delete(set, fruits[l])
+			}
+			l += 1
+		}
+		res = util.MaxInt(res, r-l+1)
+	}
+	return res
+}
+
+func main() {
+
+}
